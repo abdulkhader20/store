2d620869bdf58d679d43fbd44a85397eca9698a2
abdulkhader20 <115452919+abdulkhader20@users.noreply.github.com>
2023-04-18 08:47:01 +0530

    upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+    var productImg=document.getElementById("productImg");
+    var samllImg=document.getElementsByClassName("small-img");
+    SmallImg[0].onclick=function()
+    {
+        productImg.src=smallImg[0].src;
+    }
+    SmallImg[1].onclick=function()
+    {
+        productImg.src=smallImg[1].src;
+    }
+    SmallImg[2].onclick=function()
+    {
+        productImg.src=smallImg[2].src;
+    }
+    SmallImg[3].onclick=function()
+    {
+        productImg.src=smallImg[3].src;
+    }
